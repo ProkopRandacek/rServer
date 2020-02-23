e0bce1865936405285789170a837fb439e9992f2
Prokop Randáček <prokop.randacek@gmail.com>
2020-02-23 22:52:43 +0100

    created config file

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-import datetime
-import markdown2
+import datetime, markdown2, json
+from collections import namedtuple
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class S(BaseHTTPRequestHandler):
@@ -14,13 +14,13 @@ class S(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/css")
         self.end_headers()
-        self.wfile.write(open("assets/styles.css", "r").read().encode("utf8"))
+        self.wfile.write(open(conf.paths.css, "r").read().encode("utf8"))
 
     def sendIco(self):
         self.send_response(200)
         self.send_header("Content-type", "image/png")
         self.end_headers()
-        self.wfile.write(open("assets/favicon.png", "rb").read())
+        self.wfile.write(open(conf.paths.ico, "rb").read())
 
     def sendImg(self, path):
         self.send_response(200)
@@ -38,45 +38,46 @@ class S(BaseHTTPRequestHandler):
         arg = self.path[1:].split(";")
         print("[" + str(datetime.datetime.now()) + "]", "GET request -", ":".join(map(str, self.client_address)), "- arg:", arg)
         if arg[0] == "":
-            self.send(Build("home"))
-        elif arg[0] in navbarPaths:
+            self.send(Build(conf.navbar.paths[0]))
+        elif arg[0] in conf.navbar.paths:
             self.send(Build(arg[0]))
         elif arg[0] == "css":
             self.sendCss()
         elif arg[0] in ["ico", "favicon.ico"]:
             self.sendIco()
         elif arg[0] == "setup":
-            self.sendImg("assets/setup.png")
+            self.sendImg(conf.paths.setup)
         else:
             self.send(Build("404"))
-
-navbarNames = ["Home", "My fav xkcd", "Tools", "Contact", "Portfolio", "Log"]
-navbarPaths = ["home", "xkcd",        "tools", "contact", "portfolio", "log"]
-template = open("assets/template.html", 'r').read()
 
 def Build(path):
     html = template
 
     navbar = ""
-    navbarNum = navbarPaths.index(path) if path in navbarPaths else -1
-    for i in range(len(navbarNames)):
-        navbar += "<span"
-        navbar += " class=\"current\"" if i == navbarNum else ""
-        navbar += "><a href=\""
-        navbar += navbarPaths[i]
-        navbar += "\">"
-        navbar += navbarNames[i]
-        navbar += "</a></span>"
-        navbar += "" if i == len(navbarNames) - 1 else "<span> | </span>"
+    navbarNum = conf.navbar.paths.index(path) if path in conf.navbar.paths else -1
+    for i in range(len(conf.navbar.names)):
+        navbar += conf.navbar.item.pre
+        navbar += conf.navbar.item.current if i == navbarNum else ""
+        navbar += conf.navbar.item.prepath
+        navbar += conf.navbar.paths[i]
+        navbar += conf.navbar.item.separator
+        navbar += conf.navbar.names[i]
+        navbar += conf.navbar.item.pos
+        navbar += "" if i == len(conf.navbar.names) - 1 else conf.navbar.separator
 
-    content = markdown2.markdown(open("assets/content/" + path + ".md", 'r').read())
+    content = markdown2.markdown(open(conf.paths.content + path + ".md", 'r').read())
 
-    title = navbarNames[navbarNum] if not navbarNum == -1 else path
+    title = conf.navbar.names[navbarNum] if not navbarNum == -1 else path
     
     html = html.replace("*title*", title).replace("*navbar*", navbar).replace("*content*", content)
 
     return html
 
-server_address = ("localhost", 8000)
+conf = json.loads(open('conf.json').read(), object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+
+template = open(conf.paths.template, 'r').read()
+
+server_address = (conf.address, conf.port)
+
 httpd = HTTPServer(server_address, S)
 httpd.serve_forever()
