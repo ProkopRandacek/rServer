466cb404684d092f7c897d7302474169bf9cc22e
Prokop Randáček <prokop.randacek@gmail.com>
2020-04-05 23:23:26 +0200

    Licence update

--- image.py
+++ image.py
@@ -1,3 +1,19 @@
+# randacek.dev - my personal webserver software
+# Copyright (C) 2020  Prokop Randáček
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import time, random, json
 from collections import namedtuple
 from log import log
@@ -16,7 +32,7 @@ class image():
 
     def delete(self):
         log(f"deleting image {self.name}", True)
-        #TODO delete the file
+        os.remove(f"./{c.path.images}/{self.name}"
 
 class imageDB():
     images = [] 
@@ -62,6 +78,6 @@ class imageDB():
     def load(self):
         log("loading image DB")
         data = open(c.path.imageDB, "r").read().split("\n")
-        for i in data[:-1]: # last index is allways empty
+        for i in data[:-1]: # last item is allways empty
             self.images.append(image(i[:3], int(i[4:])))
         log(["loading images done", f"{len(self.images)} images loaded"])
--- requestHandler.py
+++ requestHandler.py
@@ -1,3 +1,19 @@
+# randacek.dev - my personal webserver software
+# Copyright (C) 2020  Prokop Randáček
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import markdown2, re, ssl
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from log import log
